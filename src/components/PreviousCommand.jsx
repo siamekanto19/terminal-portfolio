a8efa32dc3e2394dc40ecb1adfb9aa4ca481f0e7
Siam <71663224+siamekanto19@users.noreply.github.com>
2021-03-26 21:39:31 +0600

    Update PreviousCommand.jsx

--- src/components/PreviousCommand.jsx
+++ src/components/PreviousCommand.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function PreviousCommand(props) {
     return (
         <div className="mt-2 lg:text-lg text-green-500 flex items-center">
-            <h1>root@siamekanto.com:~$ <span className="text-blue-500">{props.command}</span> </h1>
+            <h1>root@siam.netlify.app:~$ <span className="text-blue-500">{props.command}</span> </h1>
         </div>
     )
 }
